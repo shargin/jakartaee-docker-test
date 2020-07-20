e1582765c9fc1cb656964be538ab5fd45de30859
Shargin Andrey <sharginandrei@gmail.com>
2020-07-20 15:36:47 +0300

    return hostname

--- src/main/java/org/shargin/testkuber/controllers/TestController.java
+++ src/main/java/org/shargin/testkuber/controllers/TestController.java
@@ -2,11 +2,13 @@ package org.shargin.testkuber.controllers;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 @Path("")
 public class TestController {
     @GET
-    public String test() {
-        return "test response";
+    public String test() throws UnknownHostException {
+        return InetAddress.getLocalHost().getHostName();
     }
 }
